fbdebb7a80c52fd7760d1a518cb7f8331120d595
Christopher Ariza <ariza@flexatone.com>
2025-01-07 17:39:31 -0800

    clippy!

--- src/scan_fs.rs
+++ src/scan_fs.rs
@@ -35,13 +35,12 @@ pub(crate) enum Anchor {
 }
 
 //------------------------------------------------------------------------------
-/// Given a path to a Python binary, call out to Python to get all known site packages; some site packages may not exist; we do not filter them here. This will include "dist-packages" on Linux. If `force_usite` is false, we use ENABLE_USER_SITE to determine if we should include the user site packages; if `force_usite` is true, we always include usite.
-
 const PY_SITE_PACKAGES: &str = "import site;print(site.ENABLE_USER_SITE);print(\"\\n\".join(site.getsitepackages()));print(site.getusersitepackages())";
 
+/// Given a path to a Python binary, call out to Python to get all known site packages; some site packages may not exist; we do not filter them here. This will include "dist-packages" on Linux. If `force_usite` is false, we use ENABLE_USER_SITE to determine if we should include the user site packages; if `force_usite` is true, we always include usite.
 fn get_site_package_dirs(executable: &Path, force_usite: bool) -> Vec<PathShared> {
     // let py = "import site;print(site.ENABLE_USER_SITE);print(\"\\n\".join(site.getsitepackages()));print(site.getusersitepackages())";
-    return match Command::new(executable)
+    match Command::new(executable)
         .arg("-c")
         .arg(PY_SITE_PACKAGES)
         .output()
@@ -70,7 +69,7 @@ fn get_site_package_dirs(executable: &Path, force_usite: bool) -> Vec<PathShared
             eprintln!("Failed to execute command with {:?}: {}", executable, e); // log this
             Vec::with_capacity(0)
         }
-    };
+    }
 }
 
 // Given a package directory, collect the name of all packages.
