0650bf427c0989d370ea1202eb3a6ba88903c947
Christopher Ariza <ariza@flexatone.com>
2025-02-02 12:10:42 -0800

    fix typo

--- src/scan_fs.rs
+++ src/scan_fs.rs
@@ -47,7 +47,7 @@ pub(crate) enum Anchor {
 //------------------------------------------------------------------------------
 const PY_SITE_PACKAGES: &str = "import site;import sys;import os;print(sys.prefix == sys.base_prefix and site.check_enableusersite() and not os.getenv('PYTHONNOUSERSITE'));print(\"\\n\".join(site.getsitepackages()));print(site.getusersitepackages())";
 
-/// Given a path to a Python binary, call out to Python to get all known site packages; some site packages may not exist; we do not filter them here. This will include "dist-packages" on Linux. If `force_usite` is false, we use ENABLE_USER_SITE to determine if we should include the user site packages; if `force_usite` is true, we always include usite. Using `-S`  avvoids loading site and calling sitecustomize.py, but obligates us to derive `ENABLE_USER_SITE` manually.
+/// Given a path to a Python binary, call out to Python to get all known site packages; some site packages may not exist; we do not filter them here. This will include "dist-packages" on Linux. If `force_usite` is false, we use ENABLE_USER_SITE to determine if we should include the user site packages; if `force_usite` is true, we always include usite. Using `-S` avoids loading site and calling sitecustomize.py, but obligates us to derive `ENABLE_USER_SITE` manually.
 fn get_site_package_dirs(executable: &Path, force_usite: bool) -> Vec<PathShared> {
     match Command::new(executable)
         .arg("-S")
