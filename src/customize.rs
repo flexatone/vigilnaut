2f44d40a0399a101f8442c5d7cab1a8345ae13f0
Christopher Ariza <ariza@flexatone.com>
2025-02-03 19:29:23 -0800

    updated interfaces

--- src/customize.rs
+++ src/customize.rs
@@ -1,10 +1,11 @@
+use crate::path_shared::PathShared;
 use crate::validation_report::ValidationFlags;
 use std::path::PathBuf;
 
 /// Produce the command line argument to reproduce a validation command.
-pub fn to_validation_command(
-    executable: PathBuf, // only accept one
-    bound: PathBuf,
+fn to_validation_command(
+    executable: &PathBuf, // only accept one
+    bound: &PathBuf,
     bound_options: Option<Vec<String>>,
     vf: ValidationFlags,
     exit_else_warn: Option<i32>,
@@ -29,9 +30,9 @@ pub fn to_validation_command(
     )
 }
 
-pub(crate) fn to_validation_subprocess(
-    executable: PathBuf,
-    bound: PathBuf,
+fn to_validation_subprocess(
+    executable: &PathBuf,
+    bound: &PathBuf,
     bound_options: Option<Vec<String>>,
     vf: ValidationFlags,
     exit_else_warn: Option<i32>,
@@ -42,6 +43,16 @@ pub(crate) fn to_validation_subprocess(
         "from subprocess import run;run('{}'.split(' '){})",
         cmd, eew
     )
+}
+
+pub(crate) fn to_sitecustomize(
+    executable: &PathBuf,
+    bound: &PathBuf,
+    bound_options: Option<Vec<String>>,
+    vf: ValidationFlags,
+    exit_else_warn: Option<i32>,
+    site: &PathShared,
+) {
 }
 
 //------------------------------------------------------------------------------
@@ -58,7 +69,7 @@ mod tests {
             permit_superset: false,
             permit_subset: true,
         };
-        let post = to_validation_command(exe, bound, bound_options, vf, None);
+        let post = to_validation_command(&exe, &bound, bound_options, vf, None);
         assert_eq!(
             post,
             "fetter -e python3 validate --bound requirements.txt --subset"
@@ -73,7 +84,7 @@ mod tests {
             permit_superset: true,
             permit_subset: true,
         };
-        let post = to_validation_command(exe, bound, bound_options, vf, None);
+        let post = to_validation_command(&exe, &bound, bound_options, vf, None);
         assert_eq!(post, "fetter -e python3 validate --bound requirements.txt --bound_options foo bar --subset --superset")
     }
     #[test]
@@ -86,7 +97,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = Some(4);
-        let post = to_validation_command(exe, bound, bound_options, vf, ec);
+        let post = to_validation_command(&exe, &bound, bound_options, vf, ec);
         assert_eq!(post, "fetter -e python3 validate --bound requirements.txt --bound_options foo bar --subset --superset exit --code 4")
     }
     //--------------------------------------------------------------------------
@@ -101,7 +112,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = Some(4);
-        let post = to_validation_subprocess(exe, bound, bound_options, vf, ec);
+        let post = to_validation_subprocess(&exe, &bound, bound_options, vf, ec);
         assert_eq!(post, "from subprocess import run;run('fetter -e python3 validate --bound requirements.txt --subset exit --code 4'.split(' '), check=True)")
     }
 
@@ -115,7 +126,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = None;
-        let post = to_validation_subprocess(exe, bound, bound_options, vf, ec);
+        let post = to_validation_subprocess(&exe, &bound, bound_options, vf, ec);
         assert_eq!(post, "from subprocess import run;run('fetter -e python3 validate --bound requirements.txt --subset'.split(' '))")
     }
 }
