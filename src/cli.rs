ed858f8536be70977baa91544d8dbd670dd9b4f7
Christopher Ariza <ariza@flexatone.com>
2025-02-03 20:09:23 -0800

    progress in to_customize

--- src/cli.rs
+++ src/cli.rs
@@ -13,6 +13,7 @@ use std::sync::{
 use std::thread;
 use std::time::Duration;
 
+use crate::customize::to_sitecustomize;
 use crate::dep_manifest::DepManifest;
 use crate::scan_fs::Anchor;
 use crate::scan_fs::ScanFS;
@@ -535,27 +536,43 @@ where
             superset,
             subcommands,
         }) => {
-            let dm = get_dep_manifest(bound, bound_options.as_ref())?;
-            let permit_superset = *superset;
-            let permit_subset = *subset;
-            // let vr = sfs.to_validation_report(
-            //     dm,
-            //     ValidationFlags {
-            //         permit_superset,
-            //         permit_subset,
-            //     },
-            // );
-            match subcommands {
-                Some(CustomizeInstallSubcommand::Warn) | None => {
-                    // default
-                    // let _ = vr.to_stdout();
-                    println!("warn");
-                }
-                Some(CustomizeInstallSubcommand::Exit { code }) => {
-                    // process::exit(if vr.len() > 0 { *code } else { 0 });
-                    println!("exit");
+            // let dm = get_dep_manifest(bound, bound_options.as_ref())?;
+            // let permit_superset = *superset;
+            // let permit_subset = *subset;
+            let vf = ValidationFlags {
+                permit_superset: *superset,
+                permit_subset: *subset,
+            };
+            let exit_else_warn: Option<i32> = match subcommands {
+                Some(CustomizeInstallSubcommand::Warn) | None => None,
+                Some(CustomizeInstallSubcommand::Exit { code }) => Some(*code),
+            };
+            for (exe, sites) in sfs.exe_to_sites {
+                // NOTE: should probably only write to first
+                for site in sites {
+                    let _ = to_sitecustomize(
+                        &exe,
+                        &bound,
+                        bound_options.clone(),
+                        &vf,
+                        exit_else_warn.clone(),
+                        &site,
+                    );
                 }
             }
+            // TODO: can be parallel
+            // sfs.exe_to_sites.iter().flat_map(|(exe, sites)| {
+            //     for site in sites {
+            //         let _ = to_sitecustomize(
+            //             &exe,
+            //             &bound,
+            //             bound_options.clone(),
+            //             vf,
+            //             exit_else_warn.clone(),
+            //             site,
+            //         );
+            //     }
+            // });
         }
         Some(Commands::Audit {
             subcommands,
--- src/customize.rs
+++ src/customize.rs
@@ -1,15 +1,16 @@
 use crate::path_shared::PathShared;
 use crate::validation_report::ValidationFlags;
-use std::path::PathBuf;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
+use std::path::Path;
 
 /// Produce the command line argument to reproduce a validation command.
 fn to_validation_command(
     executable: &PathBuf, // only accept one
-    bound: &PathBuf,
+    bound: &Path,
     bound_options: Option<Vec<String>>,
-    vf: ValidationFlags,
+    vf: &ValidationFlags,
     exit_else_warn: Option<i32>,
 ) -> String {
     let bo = bound_options.as_ref().map_or(String::new(), |vec| {
@@ -36,7 +37,7 @@ fn to_validation_subprocess(
     executable: &PathBuf,
     bound: &PathBuf,
     bound_options: Option<Vec<String>>,
-    vf: ValidationFlags,
+    vf: &ValidationFlags,
     exit_else_warn: Option<i32>,
 ) -> String {
     let cmd = to_validation_command(executable, bound, bound_options, vf, exit_else_warn);
@@ -51,11 +52,12 @@ pub(crate) fn to_sitecustomize(
     executable: &PathBuf,
     bound: &PathBuf,
     bound_options: Option<Vec<String>>,
-    vf: ValidationFlags,
+    vf: &ValidationFlags,
     exit_else_warn: Option<i32>,
     site: &PathShared,
 ) {
-    let code = to_validation_subprocess(executable, bound, bound_options, vf, exit_else_warn);
+    let code =
+        to_validation_subprocess(executable, bound, bound_options, vf, exit_else_warn);
     let fp = site.join("sitecustomize.py");
     let mut file = File::create(&fp).unwrap();
     writeln!(file, "{}", code).unwrap();
@@ -75,7 +77,7 @@ mod tests {
             permit_superset: false,
             permit_subset: true,
         };
-        let post = to_validation_command(&exe, &bound, bound_options, vf, None);
+        let post = to_validation_command(&exe, &bound, bound_options, &vf, None);
         assert_eq!(
             post,
             "fetter -e python3 validate --bound requirements.txt --subset"
@@ -90,7 +92,7 @@ mod tests {
             permit_superset: true,
             permit_subset: true,
         };
-        let post = to_validation_command(&exe, &bound, bound_options, vf, None);
+        let post = to_validation_command(&exe, &bound, bound_options, &vf, None);
         assert_eq!(post, "fetter -e python3 validate --bound requirements.txt --bound_options foo bar --subset --superset")
     }
     #[test]
@@ -103,7 +105,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = Some(4);
-        let post = to_validation_command(&exe, &bound, bound_options, vf, ec);
+        let post = to_validation_command(&exe, &bound, bound_options, &vf, ec);
         assert_eq!(post, "fetter -e python3 validate --bound requirements.txt --bound_options foo bar --subset --superset exit --code 4")
     }
     //--------------------------------------------------------------------------
@@ -118,7 +120,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = Some(4);
-        let post = to_validation_subprocess(&exe, &bound, bound_options, vf, ec);
+        let post = to_validation_subprocess(&exe, &bound, bound_options, &vf, ec);
         assert_eq!(post, "from subprocess import run;run('fetter -e python3 validate --bound requirements.txt --subset exit --code 4'.split(' '), check=True)")
     }
 
@@ -132,7 +134,7 @@ mod tests {
             permit_subset: true,
         };
         let ec: Option<i32> = None;
-        let post = to_validation_subprocess(&exe, &bound, bound_options, vf, ec);
+        let post = to_validation_subprocess(&exe, &bound, bound_options, &vf, ec);
         assert_eq!(post, "from subprocess import run;run('fetter -e python3 validate --bound requirements.txt --subset'.split(' '))")
     }
 }
